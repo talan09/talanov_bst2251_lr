d6054be41b70c01417dd6299168b8d1ba1c6779b
talan09 <talan.09@mail.ru>
2026-05-19 04:00:43 +0300

    Delete DjangoProject1 directory

--- DjangoProject1/flatpages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'flatpages'
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('/hello', views.home, name='home'),
-]
